37c3381335e62b7e0aecd7292cb9c6f812f33c26
Jacob Malcom <jmalcom@defenders.org>
2016-10-17 16:00:31 -0400

    Initial commit.

---
+++ README.md
@@ -0,0 +1,32 @@
+Represent content in a modal in GitBook
+==============
+
+Modified from [chudaol's GitBook YouTube plugin](https://github.com/chudaol/gitbook-plugin-modal-youtube-video).
+
+This plugins requires gitbook `>=2.0.0`.
+
+### Usage
+
+Add plugin to your `book.json`, then run `gitbook install`:
+
+```
+{
+    plugins: ["modals"]
+}
+```
+
+Add the web source for the modal - another webpage, a YouTube video, etc. - within the template tags:
+
+```
+{% modalItems %}http://www.github.com Look, here's GitHub{% endmodalYoutube %}
+```
+
+### Add custom html code to the modal header
+
+Custom html can be inserted within the template tag to appear in the modal's header. For example, if you would like a link to appear in the modal header, then you can use the pipe character to separate the URL link from the item label and from your custom html header code:
+
+```
+
+{% modalItems %}http://www.youtube.com/v/9bZkp7q19f0|Look at this video!|<a href="#">This is my custom header link!</a>{% endmodalItems %}
+```
+The text will appear left-justified in the modal header, complete with a link.
---
+++ index.js
@@ -0,0 +1,90 @@
+var _ = require("underscore");
+
+/**
+ * Parses the string and finds the parts of it depending how it is created
+ *
+ * @param {String} string
+ * @returns {Object} object which contains parts href, label, header and buttonHref
+ */
+var getParts = function (string) {
+  var hasHeader, parts, label, href, header;
+
+  hasHeader = false;
+  parts = string.split("|");
+
+  if (parts.length > 1) {
+    hasHeader = true;
+  } else {
+    parts = string.split(" ");
+  }
+
+  //Get video href
+  href = parts[0];
+  href = href.replace("watch?v=", "v/");
+
+  //Get button label
+  if (hasHeader) {
+    label = parts[1];
+  } else {
+    label = _.last(parts, parts.length - 1).join(" ");
+  }
+
+  if (_.isString(label) === false || label.length === 0) {
+    label = "Unknown source";
+  }
+  //Get header
+  header = hasHeader ? parts[2] : "";
+
+  if (_.isString(header) === false || header.length === "") {
+    header = "";
+  }
+
+  return {
+    href        : href,
+    label       : label,
+    header      : header,
+    buttonHref  : this.generator != "website" ? "#" : href
+  };
+
+};
+
+module.exports = {
+  website: {
+    assets: "./book",
+    js: [
+      "script.js"
+    ],
+    css: [
+      "style.css"
+    ]
+  },
+  blocks: {
+    modalItems: {
+      process: function(blk) {
+        var buttonHTML, modalHTML, string, parts, src, label, href;
+
+        string = blk.body;
+        parts = getParts(string);
+
+        buttonHTML = "<a href=\"" + 
+          parts.buttonHref + 
+          "\" class=\"button play\">" + 
+          parts.label + "</a>";
+
+        modalHTML =  "<div class=\"modal modal_item\">" +
+          "<div class=\"modal_content\"><div class=\"modal_header\">" + 
+          parts.header + 
+          "</div><ins class=\"close\"></ins>"  +
+          "<div class=\"emb_item\">" +
+          "<iframe width=\"100%\" height=\"100%\" src=\"" + 
+          parts.href + 
+          "\" frameborder=\"0\" allowfullscreen=\"\"></iframe>" +
+          "</div>" +
+          "</div>" +
+          "</div>";
+
+        return "<div class=\"modal_parent\">" + buttonHTML + modalHTML + "</div>";
+      }
+    }
+  }
+};
